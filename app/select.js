de59412f99ed2018ef6dc5f0852fe49563af708d
JACKYCHEN811009 <151000137+JACKYCHEN811009@users.noreply.github.com>
2023-12-03 19:33:17 +0900

    Update select.js

--- app/select.js
+++ app/select.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, Image } from 'react-native';
+import { useNavigation, useRoute } from "expo-router";
 import { Link } from "expo-router";
+import { useIsFocused } from "@react-navigation/native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Select = () => {
+  const isFocused = useIsFocused();
+  const navigation = useNavigation();
+  const [userInfo, setUserInfo] = useState(null);
+
   const [borderStyle, setBorderStyle] = useState({
     relaxation: {
       borderWidth: 0.25,
@@ -22,6 +29,32 @@ const Select = () => {
     }));
   };
 
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        // 从AsyncStorage中获取用户信息
+        const storedUserInfo = await AsyncStorage.getItem('userInfo');
+        if (storedUserInfo) {
+          const user = JSON.parse(storedUserInfo);
+          console.log("用户已登录:", user);
+          console.log("用户名:", user.firstName); // 假设用户信息中有一个名为 name 的字段
+          setUserInfo(user);
+          // 在这里执行您需要在页面聚焦时进行的操作
+        } else {
+          console.log("用户未登录");
+          // 在这里执行其他逻辑（例如，重置表单字段）
+        }
+      } catch (error) {
+        console.error('获取用户信息时发生错误:', error);
+      }
+    };
+
+    if (isFocused) {
+      console.log("页面处于焦点状态");
+      checkLoginStatus();
+    }
+  }, [isFocused]); // 只需关注 isFocused
+
   return (
     <View style={styles.container}>
       <Link href='/relaxation' onPress={() => handleLinkPress('relaxation')}>
@@ -37,6 +70,7 @@ const Select = () => {
         />
       </Link>
       <Text style={[styles.text]}>どちらを体験しますか？</Text>
+      <Link style={[styles.login]} href={"/logout"}><Text>logout</Text></Link>
     </View>
   );
 }
@@ -75,6 +109,12 @@ const styles = StyleSheet.create({
     bottom: 125,
     left: '50%',
     transform: [{ translateX: -101.25 }, { translateY: 0 }]
+  },
+  login:{
+    color: '#FFFFFF',
+    position: 'absolute',
+    bottom: 50,
+    left: '50%',
   }
 });
 
